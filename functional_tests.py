004a5f91ef710b7facb2927ff7757a1d3b36d921
ekirkegaard <kirkegaard.emily@gmail.com>
2018-03-27 11:59:44 +0200

    fixed typo

--- functional_tests.py
+++ functional_tests.py
@@ -4,7 +4,7 @@ import unittest
 class NewVisitorTest(unittest.TestCase):
 
 		def setUp(self):
-			self.browswer = webdriver.Firefox()
+			self.browser = webdriver.Firefox()
 
 		def tearDown(self):
 			self.browser.quit()
@@ -42,4 +42,3 @@ class NewVisitorTest(unittest.TestCase):
 
 if __name__ == '__main__':
 	unittest.main(warnings='ignore')
-	
